9da1ed84355e33dbc1153aa4afbb728913a2ca05
Leandro Augusto <leandroaugusto470@outlook.com>
2022-09-17 11:31:02 -0300

    feat: login form

--- pages/cart.js
+++ pages/cart.js
@@ -91,7 +91,7 @@ function CartScreen() {
               <li>
                 <button
                   className="primary-button w-full"
-                  onClick={() => router.push("/shipping")}
+                  onClick={() => router.push("login?redirect=/shipping")}
                 >
                   Checkout
                 </button>
