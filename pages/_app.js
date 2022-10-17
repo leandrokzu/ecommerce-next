ec6ab2429332aa677b040b409234243861a8ac7e
Leandro Augusto <leandroaugusto470@outlook.com>
2022-10-17 20:11:42 -0300

    feat: paypal integration

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,21 @@ import "../styles/globals.css";
 import { SessionProvider, useSession } from "next-auth/react";
 import { StoreProdiver } from "../utils/Store";
 import { useRouter } from "next/router";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProdiver>
-        {Component.auth ? (
-          <Auth>
+        <PayPalScriptProvider>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
+          )}
+        </PayPalScriptProvider>
       </StoreProdiver>
     </SessionProvider>
   );
